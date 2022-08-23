34b357ce0cfcea85fbd580d3300df5e72e980fcb
Junewoo <krca0220@gmail.com>
2022-08-23 13:52:52 +0900

    animation add

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_practice/global/routes/pages.dart';
 import 'package:riverpod_practice/presentation/provider/memo_provider.dart';
 import 'package:riverpod_practice/presentation/provider/sort_filter_provider.dart';
 import 'package:riverpod_practice/presentation/view_models/home/home_view_model.dart';
 
-class Home extends ConsumerWidget {
+class Home extends HookConsumerWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final viewModel = ref.watch(homeViewModelProvider);
+    final listAnimationController =
+        useAnimationController(duration: const Duration(milliseconds: 1000))
+          ..forward();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Memo'),
@@ -68,48 +73,65 @@ class Home extends ConsumerWidget {
                 child: CircularProgressIndicator(),
               ),
               success: (value) => ListView.separated(
-                itemBuilder: ((context, index) => Dismissible(
-                      key: ValueKey(value[index].id),
-                      background: Container(
-                          margin: const EdgeInsets.all(8),
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          alignment: Alignment.centerRight,
-                          color: Colors.red,
-                          child: const Icon(
-                            Icons.delete,
-                            size: 30,
-                            color: Colors.white,
-                          )),
-                      onDismissed: (direction) {
-                        viewModel.deleteMemo(value[index].id);
-                      },
-                      child: InkWell(
-                        child: SizedBox(
-                          height: 50,
-                          child: ListTile(
-                            title: Text(
-                              value[index].title,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                itemBuilder: ((context, index) => FadeTransition(
+                      opacity: Tween(begin: 0.0, end: 1.0).animate(
+                          CurvedAnimation(
+                              parent: listAnimationController,
+                              curve: Curves.easeOutCubic)),
+                      child: SlideTransition(
+                        position: Tween<Offset>(
+                                begin: const Offset(0, 1),
+                                end: const Offset(0, 0))
+                            .animate(CurvedAnimation(
+                                parent: listAnimationController,
+                                curve: Curves.easeInOutSine)),
+                        child: Dismissible(
+                          key: ValueKey(value[index].id),
+                          background: Container(
+                              margin: const EdgeInsets.all(8),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              alignment: Alignment.centerRight,
+                              color: Colors.red,
+                              child: const Icon(
+                                Icons.delete,
+                                size: 30,
+                                color: Colors.white,
+                              )),
+                          onDismissed: (direction) {
+                            viewModel.deleteMemo(value[index].id);
+                          },
+                          child: InkWell(
+                            child: SizedBox(
+                              height: 50,
+                              child: ListTile(
+                                title: Text(
+                                  value[index].title,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(value[index].content ?? ''),
+                                    Text(value[index]
+                                        .createdAt
+                                        .toIso8601String()),
+                                  ],
+                                ),
+                                dense: true,
                               ),
                             ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(value[index].content ?? ''),
-                                Text(value[index].createdAt.toIso8601String()),
-                              ],
-                            ),
-                            dense: true,
+                            onTap: () {
+                              GoRouter.of(context).pushNamed(
+                                RouteList.detail_page.name,
+                                params: {'id': value[index].id},
+                              );
+                            },
                           ),
                         ),
-                        onTap: () {
-                          GoRouter.of(context).pushNamed(
-                            RouteList.detail_page.name,
-                            params: {'id': value[index].id},
-                          );
-                        },
                       ),
                     )),
                 itemCount: value.length,
--- lib/presentation/view_models/home/home_view_model.dart
+++ lib/presentation/view_models/home/home_view_model.dart
@@ -1,16 +1,19 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_practice/domain/entities/memo/memo_entity.dart';
 import 'package:riverpod_practice/presentation/provider/memo_provider.dart';
 import 'package:riverpod_practice/presentation/provider/sort_filter_provider.dart';
 import 'package:riverpod_practice/presentation/state/state.dart';
 
-final homeViewModelProvider = Provider.autoDispose<HomeViewModel>(
+final homeViewModelProvider = Provider<HomeViewModel>(
   (ref) => HomeViewModel(ref),
 );
 
 class HomeViewModel {
   final Ref _ref;
   HomeViewModel(this._ref);
+
+  
 
   State<List<MemoEntity>> get memoState => _ref.watch(sortedMemoListState);
   SortOrder get orderState => _ref.watch(sortOrderProvider);
